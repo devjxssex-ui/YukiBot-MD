9ad3730f9c692ce8375cc018088fa5180a5cc261
Jxssex <devjxssex@gmail.com>
2026-04-01 11:55:09 -0600

    Actualizar gachareserved.js

--- commands/gachareserved.js
+++ commands/gachareserved.js
@@ -1,9 +1,12 @@
+// gach reserved.js - Sistema de rolls para Gacha estilo Rock Lee 🍃
 import chalk from 'chalk'
 
 const limpiarRolls = () => {
   try {
     const chats = global.db.data.chats
     const now = Date.now()
+    let eliminados = 0
+    
     for (const chatId of Object.keys(chats)) {
       const chat = chats[chatId]
       if (!chat.rolls || typeof chat.rolls !== 'object') {
@@ -16,13 +19,21 @@ const limpiarRolls = () => {
         const reclamado = roll.claimed === true
         if (expirado || reclamado) {
           delete chat.rolls[msgId]
+          eliminados++
         }
       }
     }
+    
+    if (eliminados > 0) {
+      console.log(chalk.green(`🍃 ${eliminados} rolls expirados limpiados del dojo 💚`))
+    }
   } catch (e) {
-    console.log(chalk.gray('Error limpiando rolls'))
+    console.log(chalk.gray('🍃 Error limpiando rolls'))
   }
 }
 
+// Limpiar cada 30 minutos (1800000 ms)
 setInterval(limpiarRolls, 1800000)
 limpiarRolls()
+
+console.log(chalk.cyan(`🍃 Sistema de Gacha activado - ¡Reclama tus personajes! 💚`))
